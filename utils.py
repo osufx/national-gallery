6855691e234222d946c32cc5d2a941c967602c68
Sunpy <sunpy@osufx.com>
2018-02-13 15:41:44 +0100

    Added missing function

--- utils.py
+++ utils.py
@@ -4,4 +4,9 @@ def load_achievements():
     glob.ACHIEVEMENTS_VERSION = 1
 
 def unlock_achievements(score, beatmap, user_data):
-    return
+    return -1
+
+def achievements_response(id):
+    #Achievement structure is:
+    #"+".join([name/load, title, subtitle])
+    return ""
