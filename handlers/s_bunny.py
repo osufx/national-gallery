df69a9224d2a0303774c197b83d2c0dbefe600b4
Sunpy <sunpy@osufx.com>
2018-05-30 09:47:38 +0200

    Added ppy proud moment mapping achievement

--- handlers/s_bunny.py
+++ handlers/s_bunny.py
@@ -1,11 +1,11 @@
-if __name__ != "handlers.s_maria":
+if __name__ != "handlers.s_bunny":
     	from secret.achievements import common
 	from objects import glob
 else:
 	import common
 
 VERSION = 6
-ORDER = 6
+ORDER = 7
 
 # Loads the achievement length on load
 LENGTH = 0
@@ -17,9 +17,9 @@ ACHIEVEMENT_BASE = {
 }
 
 ACHIEVEMENT_KEYS = {
-	"name": ["A meganekko approaches"],
-	"description": ["Congratulations, you met Maria!"],
-    "icon": ["mania-secret-meganekko"]
+	"name": ["Don't let the bunny distract you!"],
+	"description": ["The order was indeed, not a rabbit."],
+    "icon": ["all-secret-bunny"]
 }
 
 # Might implement something to "automate" this if there is only one achievement for this handler (Make different structure)
@@ -36,15 +36,15 @@ def load():
 	ACHIEVEMENTS, LENGTH = common.load_achievement_data(ACHIEVEMENT_BASE, ACHIEVEMENT_KEYS, ACHIEVEMENT_STRUCT)
 
 def handle(mode, score, beatmap, user_data):
-	return check(mode, score.maxCombo)
+	return check(mode, score, beatmap.beatmapSetID)
 
-def check(mode, max_combo):
-	if mode is not 3 or max_combo < 100:
+def check(mode, score, beatmapSetID):
+	if mode is not 0 or beatmapSetID is not 184 or not score.fullCombo:
 		return []
 	return [0]
 
 def update(userID):
-	found = glob.db.fetch("SELECT id FROM scores WHERE userid = %s AND play_mode = 3 AND completed >= 2 AND max_combo >= 100 LIMIT 1;", [userID])
+	found = glob.db.fetch("SELECT id FROM scores WHERE userid = %s AND play_mode = 0 AND beatmap_md5 IN ('6fe6d6e6d2fae3da30140b640c7ec49a', '642727d50438686f6f35cc1aacbcb3cd') AND completed >= 2 AND full_combo = 1 LIMIT 1;", [userID])
 	if found is None:
 		return []
 	return [0]
